d1d46e432264bf89f86c2445a167e9bd564a3944
Richard Hamel <gomericroth@gmail.com>
2024-10-30 22:34:26 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <htc.h>
+
+int main(void)
+{
+	while(1)
+	{
+	}
+	return 1;
+}
+
---
+++ main0.c
@@ -0,0 +1,23 @@
+#include <htc.h>
+
+int main(void)
+{
+	while(1)
+	{
+	}
+	return 1;
+}
+
+
+0x56fcbc19e55bc9f91c8d759f0f225c939468fb82
+
+
+Golovin Roman 02.07.2000
+STR.KAHOVSKAIA NR.18 AP.29
+
+Gmail:
+romagolovin00@gmail.com
+roman2000golovin
+
+anydesk addr. password
+1291175393 ,Babki099
